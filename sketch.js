a95a558391446b71055389283ecd05faa6318f32
alviseaspesi <alviseaspesi@gmail.com>
2019-10-06 18:50:33 +0200

    update 2

--- sketch.js
+++ sketch.js
@@ -2,12 +2,9 @@ function preload() {
   // put preload code here
 }
 
-//define the bacteria's aspect
-var bacteria;
-
 //define variable "greenBacteria" and "redBacteria"
-var greenBacteria = [];
-var redBacteria = [];
+var gB = [];
+var rB = [];
 
 //define stroke color for bacteria
 var colorList1 = ["green", "lime"];
@@ -26,34 +23,35 @@ function setup() {
   //audio.loop();
 
   //setup how many greenbacteria will display and match the greenBacteria to the goodBacteria
-  for (var i = 0; i < random(1000, 4000); i++) {
-    greenBacteria[i] = new goodBacteria();
+  for (var i = 0; i < random(2000, 4000); i++) {
+    gB[i] = new goodBacteria();
   }
 
   //setup how many redbacteria will display and match the redBacteria to the badBacteria
-  for (var i = 0; i < random(10, 50); i++) {
-    redBacteria[i] = new badBacteria();
+  for (var j = 0; j < random(10, 100); j++) {
+    rB[j] = new badBacteria();
+  }
+}
+
+function draw() {
+  background("#49BD61");
+
+  //draw the greenBacteria
+  for (var i = 0; i < gB.length; i++) {
+    gB[i].move();
+    gB[i].display();
+  }
+
+  //draw the redBacteria
+  for (var i = 0; i < rB.length; i++) {
+    rB[i].move();
+    rB[i].display();
   }
 }
 
 function mousePressed() {
-
-}
-
-
-function draw() {
-  background(color("#49BD61"));
-
-  //draw the greenBacteria
-  for (var i = 0; i < greenBacteria.length; i++) {
-    greenBacteria[i].move();
-    greenBacteria[i].display();
-  }
-
-  //draw the redBacteria
-  for (var i = 0; i < redBacteria.length; i++) {
-    redBacteria[i].move();
-    redBacteria[i].display();
+  for (var i = 0; i < rB.length; i++) {
+    rB[i].click();
   }
 }
 
@@ -61,16 +59,14 @@ function draw() {
 function goodBacteria() {
   this.x = random(0, width);
   this.y = random(0, height);
+  this.r = random(5, 10);
 
   this.display = function() {
     var index = floor(random() * colorList1.length);
     var colorS = colorList1[index];
     stroke(color(colorS));
     fill(color(0, 128, 0, 75));
-    bacteria: {
-      rect(this.x, this.y, 25, 10, 5);
-      rect(this.x + 24, this.y, 25, 10, 5);
-    }
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
   }
 
   this.move = function() {
@@ -83,24 +79,27 @@ function goodBacteria() {
 function badBacteria() {
   this.x = random(0, width),
   this.y = random(0, height),
+  this.r = random(5, 10);
 
   this.display = function() {
-      var index = floor(random() * colorList2.length);
-      var colorS = colorList2[index];
-      stroke(color(colorS));
-      fill(color(255, 0, 0, 75));
-      bacteria: {
-        rect(this.x, this.y, 20, 4, 5);
-        rect(this.x + 20, this.y, 20, 4, 5);
-      }
-    }
-
-  this.clicked = function() {
-
+    var index = floor(random() * colorList2.length);
+    var colorS = colorList2[index];
+    stroke(color(colorS));
+    fill(color(255, 0, 0, 75));
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
   }
 
   this.move = function() {
     this.x = this.x + random(-1, 1);
     this.y = this.y + random(-1, 1);
   }
+
+  this.click = function() {
+    var d = dist(mouseX, mouseY, this.x, this.y);
+    if (d < this.r) {
+      this.r = 0;
+      rB.push(new badBacteria());
+    }
+  }
+
 }
